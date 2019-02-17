cea6723abcdf8fc02a32b94833c340084474f7b2
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-17 09:45:54 +0300

    1

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -6,8 +6,28 @@ namespace PadawansTask11
     {
         public static int? FindIndex(double[] array, double accuracy)
         {
-            // put your code here
-            throw new NotImplementedException();
+            int? index = 0;
+            double sumLeft = array[0], sumRight = array[array.Length - 1];
+            for (int i = 0, y = array.Length - 1; i < y;)
+            {
+
+                if (sumLeft <= sumRight)
+                {
+                    sumLeft += array[i + 1];
+                    i++;
+                    index = i;
+                }
+                else if (sumLeft >= sumRight)
+                {
+                    sumRight += array[y - 1];
+                    y--;
+                    index = i;
+                }
+            }
+            if (Math.Abs(sumLeft - sumRight) > accuracy)
+                return null;
+            else
+                return index;
         }
     }
 }
