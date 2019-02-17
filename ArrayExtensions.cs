8d3cb02bbd751d3807bbaba10557e9754f77c329
Prystauka Yauheni <34281826+Zheka1389@users.noreply.github.com>
2019-02-17 14:09:58 +0300

    Update ArrayExtensions.cs

--- ArrayExtensions.cs
+++ ArrayExtensions.cs
@@ -18,25 +18,39 @@ namespace PadawansTask11
             {
                 throw new ArgumentException();
             }
-            int index = 0;
-            double sumLeft = array[0], sumRight = array[array.Length - 1];
-            for (int i = 0, y = array.Length - 1; i < y;)
+            else if (array.Length <= 2)
             {
-
-                if (sumLeft <= sumRight)
-                {
-                    sumLeft += array[i + 1];
-                    i++;
-                    index = i;
-                }
-                else if (sumLeft >= sumRight)
-                {
-                    sumRight += array[y - 1];
-                    y--;
-                    index = i;
-                }
+                return null;
             }
-            if (Math.Abs(sumLeft - sumRight) > accuracy)
+            int? index = 0;
+            double sumLeft = array[0], sumRight = array[array.Length - 1];
+            if (array.Length == 3)
+            {
+                if (Math.Abs(sumLeft - sumRight) >= accuracy)
+                    return null;
+                else
+                    return 1;
+            }
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                sumLeft = 0;
+                sumRight = 0;
+                for (int y = 0; y < array.Length; y++)
+                {
+                    if (y < i)
+                    {
+                        sumLeft += array[y];
+                    }
+                    else if (y > i)
+                    {
+                        sumRight += array[y];
+                    }
+                }
+                if (Math.Abs(sumLeft - sumRight) <= accuracy)
+                    index = i;
+            }
+
+            if (index == 0)
                 return null;
             else
                 return index;
